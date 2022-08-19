b8b0ae322af7d8940c4560f49789d2a30b42cc21
unknown <abdelkarim01.ma@gmail.com>
2022-08-19 20:52:18 +0100

    add security features :registration and login

--- src/main/java/tacos/repositories/IngredientRepository.java
+++ src/main/java/tacos/repositories/IngredientRepository.java
@@ -1,8 +1,10 @@
 package tacos.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import tacos.entities.Ingredient;
+
 
 public interface IngredientRepository extends CrudRepository<Ingredient, String> {
 
--- src/test/java/tacos/TacoAppApplicationTests.java
+++ src/test/java/tacos/TacoAppApplicationTests.java
@@ -3,11 +3,13 @@ package tacos;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class TacoAppApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+	
 
 }
